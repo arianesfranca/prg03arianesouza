6fb1cc6cf4d4b23805780ae4926d3135e955f3db
arianesfranca <ariianesfranca@gmail.com>
2026-09-14 21:14:06 -0300

    instancia objeto Usuario no login e atualiza label com dados do objeto

--- src/main/java/br/com/ifba/login/view/TelaLogin.java
+++ src/main/java/br/com/ifba/login/view/TelaLogin.java
@@ -4,6 +4,8 @@
  */
 package br.com.ifba.login.view;
 
+import br.com.ifba.usuario.entity.Usuario;
+
 /**
  *
  * @author ariia
@@ -125,14 +127,20 @@ public class TelaLogin extends javax.swing.JFrame {
 
     private void btnEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntrarActionPerformed
         // 1. captura o que foi digitado nos campos
-String loginDigitado = txtLogin.getText();
-String senhaDigitada = new String(txtSenha.getPassword());
+        String loginDigitado = txtLogin.getText();
+        String senhaDigitada = new String(txtSenha.getPassword());
+        
+         // 2. instancia o objeto de dominio e preenche login e senha
+        Usuario usuario = new Usuario();
+        usuario.login = loginDigitado;
+        usuario.senha = senhaDigitada;
 
-// 2. usa as variáveis para preencher o label
-lblResultado.setText(
-    "Login digitado: " + loginDigitado + "\n" +
-    "Senha digitada: " + senhaDigitada
-);
+
+        /// 3. usa os dados vindos do objeto para preencher o label
+            lblResultado.setText(
+              "Login digitado: " + usuario.login + "\n" +
+                "Senha digitada: " + usuario.senha
+    );
     }//GEN-LAST:event_btnEntrarActionPerformed
 
     /**
